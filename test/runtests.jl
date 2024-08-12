ff7fc7dedc297987948753fb1083ba668ca80220
Jeet Sukumaran <jeetsukumaran@gmail.com>
2024-08-12 16:22:24 -0700

    Update

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using DendroPy
 using Test
+using JSON
+
+function get_test_data()
+    source_path = joinpath(@__DIR__, "data", "expectedresults20240812.json")
+    return JSON.parsefile(source_path)
+end
 
 @testset "DendroPy.jl" begin
     # Write your tests here.
+    test_data = get_test_data()
+    foreach(test_data["trees"]) do tree_data
+        @info tree_data["newick"]
+    end
 end
