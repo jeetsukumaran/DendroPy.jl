f3b181b28d65bbf120a903ded8c75b9327b6126b
Jeet Sukumaran <jeetsukumaran@gmail.com>
2024-08-12 16:07:24 -0700

    Update

--- src/DendroPy.jl
+++ src/DendroPy.jl
@@ -1,6 +1,7 @@
 module DendroPy
 
-export dendropy
+export dendropy,
+       divergence_times_from_dendropy_tree
 
 import PyCall
 
@@ -8,6 +9,26 @@ const dendropy = PyCall.PyNULL()
 
 function __init__()
     copy!(dendropy, PyCall.pyimport("dendropy"))
+end
+
+function divergence_times_from_dendropy_tree(
+    tree,
+    is_include_leaves::Bool = false,
+)
+    nd_ages = Dict{typeof(tree.seed_node), Float64}()
+    # nd_ages = Dict{Int64, Float64}()
+    # nd_key = (nd) -> PyCall.py"""id($nd)"""
+    nd_key = (nd) -> nd
+    for cnd in tree.preorder_node_iter()
+        cnd_key = nd_key(cnd)
+        node_edge_length = (cnd.edge != nothing && cnd.edge.length != nothing) ? cnd.edge.length : 0.0
+        if (cnd.parent_node != nothing)
+        #     nd_ages[cnd_key] = nd_ages[nd_key(cnd.parent_node)] + node_edge_length
+        elseif (is_include_leaves || !cnd.is_leaf())
+            nd_ages[cnd_key] = node_edge_length
+        end
+    end
+    return values(nd_ages)
 end
 
 end
