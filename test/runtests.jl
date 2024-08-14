7bdb74977fd4b988c30e40320d7dec32665c70ee
Jeet Sukumaran <jeetsukumaran@gmail.com>
2024-08-13 22:57:53 -0700

    Update

--- test/runtests.jl
+++ test/runtests.jl
@@ -36,22 +36,20 @@ function check_mapping_over_tree()
     test_newick_str = test_d["definitions"]["newick"]
     DendroPy.enumerate_map_trees( (tree_idx, tree) -> begin
         test_tree_data = test_trees_data[tree_idx]
-        postorder_nodes = DendroPy.postorder_iter(tree)
-        preorder_nodes = DendroPy.preorder_iter(tree)
-        postorder_labels = DendroPy.postorder_map( (nd) -> nd.data.taxon === nothing ? nd.data.label : nd.data.taxon.label, tree )
-        preorder_labels = DendroPy.preorder_map( (nd) -> nd.data.taxon === nothing ? nd.data.label : nd.data.taxon.label, tree )
-        # @test postorder_keys == test_tree_data["nodes"]["keys"]["postorder"]
+        for (fn_key, apply_fn) in (
+            ["keys", (nd) -> nd.data.edge.length,],
+        )
+            for (traversal_key, iter_fn, expected_value) in (
+                # ["postorder", DendroPy.postorder_map, test_tree_data[tree_idx]["nodes"][fn_key]["postorder"]],
+                ["postorder", DendroPy.postorder_map, "x"],
+            )
+                result = iter_fn(apply_fn, tree)
+            end
+        end
     end, test_newick_str, "string", :newick)
 end
 
 @testset "DendroPy.jl: mappings over collections of trees" begin
-    # "labels": {
-    #            "all_preorder": ["X0", "A", "B", "X1", "C", "X2", "D", "E"],
-    #            "all_postorder": ["A", "B", "C", "D", "E", "X2", "X1", "X0"],
-    #            "leaves_only": ["A", "B", "C", "D", "E"],
-    #            "internal_preorder": ["X0", "X1", "X2"],
-    #            "internal_postorder": ["X2", "X1", "X0"]
-    #           },
     check_mapping_over_collection(DendroPy.enumerate_map_trees)
     check_mapping_over_collection(DendroPy.map_trees)
 end
