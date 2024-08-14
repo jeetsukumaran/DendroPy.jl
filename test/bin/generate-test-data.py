eb855f6da3dd05e45ad811b5c992192dfe776e40
Jeet Sukumaran <jeetsukumaran@gmail.com>
2024-08-13 18:49:01 -0700

    Update

--- test/bin/generate-test-data.py
+++ test/bin/generate-test-data.py
@@ -6,18 +6,8 @@ import dendropy
 import random
 import json
 
-def extract_tree_features(tree):
-    d = {}
-    for feature_class in (
-        ( "edge_lengths", lambda nd: nd.edge.length if nd.edge.length else 0.0, ),
-        ( "labels", lambda nd: nd.taxon.label if nd.taxon else nd.label, ),
-        ( "node_ages", lambda nd: nd.age, ),
-    ):
-        d[feature_class] = {}
-    return d
-
 def add_tree_features_(tree):
-    tree = add_internal_labels_(tere)
+    tree = add_internal_labels_(tree)
     return tree
 
 def add_internal_labels_(tree):
@@ -31,19 +21,29 @@ def add_internal_labels_(tree):
 def export_tree_features(out, d):
     pass
 
+def extract_tree_features(tree):
+    d = {}
+    for feature_class in (
+        ( "edge_lengths", lambda nd: nd.edge.length if nd.edge.length else 0.0, ),
+        ( "labels", lambda nd: nd.taxon.label if nd.taxon else nd.label, ),
+        ( "node_ages", lambda nd: nd.age, ),
+    ):
+        d[feature_class] = {}
+    return d
+
 def generate_test_data_trees(
-    random_seed: None,
+    out,
+    random_seed=None,
 ):
-    rng = random.Random(random_seed=None)
+    rng = random.Random(random_seed)
     tree_idx = 1
     for leaf_set_size in range(2, 24):
         leaf_labels = [f"T{idx:03d}" for idx in range(leaf_set_size)]
-        tns = DendroPy.TaxonNamespace(leaf_labels)
-        tree = pure_kingman_tree(tns)
-        tree = add_tree_features(tree)
+        tns = dendropy.TaxonNamespace(leaf_labels)
+        tree = dendropy.simulate.pure_kingman_tree(tns)
+        tree = add_tree_features_(tree)
         features = extract_tree_features(tree)
         export_tree_features(out, features)
 
-
-generate_test_data_trees()
+generate_test_data_trees(sys.stdout)
 
