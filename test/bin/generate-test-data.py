51a547a28c9a2d9b87d403e24bcf4c8044f34df4
Jeet Sukumaran <jeetsukumaran@gmail.com>
2024-08-13 20:36:34 -0700

    Update

--- test/bin/generate-test-data.py
+++ test/bin/generate-test-data.py
@@ -9,33 +9,45 @@ import json
 def add_tree_features_(tree, tree_idx):
     tree.key = f"Tree{tree_idx:03d}"
     tree = add_node_data(tree, tree.key)
+    tree.resolve_node_ages()
+    tree.resolve_node_depths()
     return tree
 
 def add_node_data(tree, tree_key):
     ind_index = 1
     for nd_idx, nd in enumerate(tree):
-        nd.key = ".".join([tree_key, f"Node{nd_idx+1}"])
+        nd.key = ".".join([tree_key, f"Node{nd_idx+1:03d}"])
         if nd._child_nodes:
             nd.label = f"I{ind_index:03d}"
             ind_index += 1
     return tree
 
 def extract_tree_features(tree):
-    d = {}
+    tree_d = {}
+    tree_d["key"] = tree.key
+    tree_d["nodes"] = {}
     for feature_class in (
         ( "keys", lambda nd: nd.key, ),
         ( "edge_lengths", lambda nd: nd.edge.length if nd.edge.length else 0.0, ),
         ( "labels", lambda nd: nd.taxon.label if nd.taxon else nd.label, ),
-        ( "node_ages", lambda nd: nd.age, ),
+        ( "ages", lambda nd: nd.age, ),
+        ( "depths", lambda nd: nd.depth, ),
+        ( "parents", lambda nd: [nd.parent_node.key] if nd.parent_node else [], ),
+        ( "children", lambda nd: [chnd.key for chnd in nd.child_nodes()] if not nd.is_leaf else [], ),
     ):
         feature_key = feature_class[0]
         feature_fn = feature_class[1]
-        d[feature_key] = {}
-        d[feature_key]["postorder"] = [feature_fn(nd) for nd in tree.postorder_node_iter()]
-        d[feature_key]["preorder"] = [feature_fn(nd) for nd in tree.preorder_node_iter()]
-        d[feature_key]["leaves"] = [feature_fn(nd) for nd in tree.leaf_nodes()]
-        d[feature_key]["internal"] = [feature_fn(nd) for nd in tree.internal_nodes()]
-    return d
+        tree_d["nodes"][feature_key] = {}
+        tree_d["nodes"][feature_key]["postorder"] = [feature_fn(nd) for nd in tree.postorder_node_iter()]
+        tree_d["nodes"][feature_key]["preorder"] = [feature_fn(nd) for nd in tree.preorder_node_iter()]
+        tree_d["nodes"][feature_key]["leaves"] = [feature_fn(nd) for nd in tree.leaf_nodes()]
+        tree_d["nodes"][feature_key]["internal"] = [feature_fn(nd) for nd in tree.internal_nodes()]
+    tree_d["features"] = {
+        "length": sum(edge.length if edge.length else 0.0 for edge in tree.postorder_edge_iter()),
+        "n_leaves": sum(1 for nd in tree.leaf_node_iter()),
+        "n_internal": sum(1 for nd in tree.internal_nodes())
+    }
+    return tree_d
 
 def export_tree_features(out, d):
     print(d)
@@ -47,14 +59,17 @@ def generate_test_data_trees(
 ):
     rng = random.Random(random_seed)
     tree_idx = 1
+    d = {}
+    d["trees"] = []
     for leaf_set_size in range(2, 24):
         leaf_labels = [f"T{idx:03d}" for idx in range(leaf_set_size)]
         tns = dendropy.TaxonNamespace(leaf_labels)
         tree = dendropy.simulate.pure_kingman_tree(tns)
         tree = add_tree_features_(tree, tree_idx)
         features = extract_tree_features(tree)
-        export_tree_features(out, features)
+        d["trees"].append(features)
         tree_idx += 1
+    json.dump(d, out)
 
 generate_test_data_trees(sys.stdout)
 
