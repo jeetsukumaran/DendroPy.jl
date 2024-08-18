6d89a2cc9084074d8032b1bda01a33c75de990af
Jeet Sukumaran <jeetsukumaran@gmail.com>
2024-08-18 03:52:27 -0700

    Update

--- src/simulate.jl
+++ src/simulate.jl
@@ -2,18 +2,27 @@
 
 
 
-    using PythonCall
+using PythonCall
 
-    function birth_death_divergence_times(
-        args... ; kwargs...
-    )
-        return divergence_times(birth_death_tree(args...; kwargs...))
-    end
+function birth_death_coalescence_ages(
+    args... ; kwargs...
+)
+    return coalescence_ages(birth_death_tree(args...; kwargs...))
+end
 
-    function birth_death_tree(
-        args... ; kwargs...
-    )
-        bd = PythonCall.pyimport("dendropy.model.birthdeath")
-        tree = bd.birth_death_tree(args...;kwargs...)
-        return abstract_tree(tree)
-    end
+function birth_death_divergence_times(
+    args... ; kwargs...
+)
+    return divergence_times(birth_death_tree(args...; kwargs...))
+end
+
+function birth_death_tree(
+    birth_rate::Real=1,
+    death_rate::Real=0,
+    args... ;
+    kwargs...
+)
+    bd = PythonCall.pyimport("dendropy.model.birthdeath")
+    tree = bd.birth_death_tree(birth_rate, death_rate, args...;kwargs...)
+    return abstract_tree(tree)
+end
