2d6cde484cc078bdad058517779f2bfe72896c15
Documenter.jl <documenter@juliadocs.github.io>
2025-12-10 23:09:33 +0000

    build based on ec30b7f

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#DendroPy","page":"Home","title":"DendroPy","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DendroPy.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#DendroPy","page":"Home","title":"DendroPy","text":"Documentation for DendroPy.\n\n","category":"section"}]
 }
